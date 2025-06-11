3b32b46a859561f77fd885170379b983261fc422
melodic-n <welcomtochillis@gmail.com>
2025-06-11 13:07:55 +0100

    inital commit

---
+++ src/main/java/com/example/Detectionsys/DetectionsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.Detectionsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectionsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Detectionsys/DetectionsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Detectionsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
